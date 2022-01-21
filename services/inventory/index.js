0eb8d6112c8c6a320e08fe1b9bb71e6b7842f7be
mrcolorblind <ja1antoniolp@gmail.com>
2022-01-21 07:07:52 -0300

    Tarefa prática #1 - Microservices

--- services/inventory/index.js
+++ services/inventory/index.js
@@ -15,10 +15,18 @@ const server = new grpc.Server();
 
 // implementa os métodos do InventoryService
 server.addService(inventoryProto.InventoryService.service, {
+    
     searchAllProducts: (_, callback) => {
         callback(null, {
             products: products,
         });
+    },
+
+    SearchProductByID: (payload, callback) => {
+        callback(
+            null,
+            products.find((product) => product.id == payload.request.id)
+        );
     },
 });
 
